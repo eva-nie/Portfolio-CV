91023bc79efa1e13d2272248702982c1bac78bc2
e-nie <99044956+e-nie@users.noreply.github.com>
2024-03-28 00:58:45 +0100

    add Main-3

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import photo from "../../../assets/images/main-photo.png"
 import { FlexWrapper } from "components/FlexWrapper"
 import { Container } from "components/Container"
 import { theme } from "styles/Theme"
+import { font } from "styles/Common"
 
 export const Main = () => {
   return (
     <StyledMain>
       <Container>
-        <FlexWrapper align="center" justify="space-between" wrap="wrap">
+        <FlexWrapper align="center" justify="space-around" wrap="wrap">
           <div>
             <SmallText>Hi There</SmallText>
             <Name>
@@ -32,22 +33,18 @@ const StyledMain = styled.section`
   display: flex;
 `
 const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
-  line-height: 41px;
-  margin: 10px;
+  ${font({ weight: 400, Fmax: 27, Fmin: 20 })}
 `
 
 const Name = styled.h2`
-  font-family: "Josefin Sans", sans-serif;
-  font-size: 50px;
-  font-weight: 700;
+  ${font({ family: "Josefin Sans, sans-serif", weight: 700, Fmax: 50, Fmin: 36 })}
   letter-spacing: 0.05em;
   margin: 10px 0;
 
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
 
     &::before {
       position: absolute;
@@ -61,6 +58,10 @@ const Name = styled.h2`
       background-color: ${theme.colors.accent};
     }
   }
+
+  @media${theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
 `
 
 const SmallText = styled.span`
@@ -72,6 +73,7 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
 
   @media ${theme.media.mobile} {
     width: 310px;
@@ -81,7 +83,8 @@ const Photo = styled.img`
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
-  right: 30px;
+  //right: 30px;
+  margin-top: 65px;
 
   &::before {
     content: "";
@@ -92,9 +95,12 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+
     @media ${theme.media.mobile} {
       width: 314px;
       height: 414px;
+      top: -17px;
+      left: 20px;
     }
   }
 `
