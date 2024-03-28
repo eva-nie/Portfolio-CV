9c453ecb709154459bfe52901b0885d111d37ecb
e-nie <99044956+e-nie@users.noreply.github.com>
2024-03-28 23:36:22 +0100

    add Skills-3

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,13 +1,10 @@
 import styled from "styled-components"
 import { theme } from "styles/Theme"
+import { font } from "styles/Common"
 
 export const SectionTitle = styled.h2`
+  ${font({ family: " Josefin Sans,    sans-serif", weight: 600, Fmax: 36, Fmin: 30 })}
   text-align: center;
-  font-family:
-    Josefin Sans,
-    sans-serif;
-  font-size: 36px;
-  font-weight: 600;
   letter-spacing: 5px;
   position: relative;
   margin-bottom: 90px;
@@ -22,5 +19,9 @@ export const SectionTitle = styled.h2`
     left: 50%;
     bottom: -30px;
     transform: translate(-50%);
+
+    @media  ${theme.media.mobile} {
+        bottom: -24px;
+      
   }
 `
