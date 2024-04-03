cc4d89ec76814cbf9d992c7daaa5abf1cc5ef98c
e-nie <99044956+e-nie@users.noreply.github.com>
2024-04-03 23:55:37 +0200

    add burger menu(useState)

--- src/layout/header/HeaderMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/HeaderMenu/mobileMenu/MobileMenu.tsx
@@ -1,14 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import { Menu } from "../menu/Menu"
 import { S } from "../HeaderMenu_Styles"
 
 export const MobileMenu: React.FC<{ menuItems: string[] }> = (props: { menuItems: string[] }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  const onBurgerBtnClick = () => setMenuIsOpen(!menuIsOpen)
+
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
         <span></span>
       </S.BurgerButton>
-      <S.MobileMenuPopup isOpen={false}>
+      <S.MobileMenuPopup
+        isOpen={menuIsOpen}
+        onClick={() => {
+          setMenuIsOpen(false)
+        }}
+      >
         <Menu menuItems={props.menuItems} />
       </S.MobileMenuPopup>
     </S.MobileMenu>
