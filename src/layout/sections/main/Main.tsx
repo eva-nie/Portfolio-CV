a1195a976f83807abeec90d21042ef9bfac95a99
e-nie <99044956+e-nie@users.noreply.github.com>
2024-04-08 23:07:17 +0200

    add typeWriter effect

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,8 @@ import photo from "../../../assets/images/main-photo.png"
 import { FlexWrapper } from "components/FlexWrapper"
 import { Container } from "components/Container"
 import { S } from "../main/Main_Styles"
+import Typewriter from "typewriter-effect"
+
 export const Main: React.FC = () => {
   return (
     <S.Main>
@@ -13,7 +15,18 @@ export const Main: React.FC = () => {
             <S.Name>
               I am <span>Svetlana Dyablo</span>
             </S.Name>
-            <S.MainTitle>A Web Developer. </S.MainTitle>
+            {/*<S.MainTitle>A Web Developer. </S.MainTitle>*/}
+            <S.MainTitle>
+              <p>A Web Developer</p>
+              <Typewriter
+                options={{
+                  strings: ["A Web Developer.", "A Frontend Developer", "A Web Designer"],
+                  autoStart: true,
+                  loop: true,
+                  delay: 150,
+                }}
+              />
+            </S.MainTitle>
           </div>
           <S.PhotoWrapper>
             <S.Photo src={photo} alt="" />
